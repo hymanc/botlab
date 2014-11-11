5b7d8b7dc6b7387f13a3a2ed707175f61798ea0a
Ryan Eustice <eustice@umich.edu>
2014-11-11 18:14:37 -0500

    wip on refactoring java subdir to support plugins

--- setenv.sh
+++ setenv.sh
@@ -2,4 +2,4 @@
 
 export VX_SHADER_PATH=$PWD/src/vx/shaders
 export VX_FONT_PATH=$PWD/src/vx/fonts
-export CLASSPATH=$CLASSPATH:$PWD/java/lcmtypes.jar
+export CLASSPATH=$CLASSPATH:$PWD/java/rob550.jar

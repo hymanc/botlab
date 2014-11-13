a27a45195f8ff734104f09203488c668088d4b7e
Ryan Eustice <eustice@umich.edu>
2014-11-13 15:34:21 -0500

    Updated setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -2,4 +2,5 @@
 
 export VX_SHADER_PATH=$PWD/src/vx/shaders
 export VX_FONT_PATH=$PWD/src/vx/fonts
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/lib
 export CLASSPATH=$CLASSPATH:$PWD/java/rob550.jar
